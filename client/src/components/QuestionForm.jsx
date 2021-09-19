c2da98589e9eaf0e2eed27c9e10f179dea04d1ac
georgekara13 <george_kara_13@hotmail.com>
2021-09-19 18:16:29 +0300

    Complete the question form

--- client/src/components/QuestionForm.jsx
+++ client/src/components/QuestionForm.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const QuestionForm = () => {
-  return <div>question form</div>;
+  const [answer, setAnswer] = useState("");
+  const [error, setError] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const submitAnswer = () => {
+    if (!error) {
+      axios
+        .post("http://localhost:3001/api/addanswer", { value: answer })
+        .then((res) => {
+          if (res.data.post === true) {
+            setSubmitted(true);
+            setAnswer("");
+          }
+        })
+        .catch((err) => setError("Something's wrong, please try again later"));
+    }
+  };
+
+  return (
+    <div className="w-full flex flex-col">
+      <h2 className="text-3xl bold mb-4">Is a hot dog a sandwich? Why?</h2>
+      <textarea
+        className={`resize-none border border-solid px-4 leading-10 rounded-md w-full lg:w-2/4 border-gray-500 ${
+          error ? "outline-none ring-1 ring-red-500" : "focus:outline-none focus:ring-1 focus:ring-gray-700"
+        }`}
+        rows={5}
+        onChange={(e) => {
+          setSubmitted(false);
+          const value = e.target.value;
+          const regex = /^\s*(yes|I\s*don'?t\s*know|no)\s*$/i;
+          if (!value || value.match(/^\s*$/)) {
+            setError("Please enter some text");
+          } else if (value.match(regex)) {
+            setError("Answer cannot be one of: yes/no/I don't know");
+          } else {
+            setError("");
+          }
+          setAnswer(e.target.value);
+        }}
+        value={answer}
+        placeholder="Please enter your response"
+        maxLength={1000} /* Assuming that there aren't any strongly opinionated respondents on the subject,
+        and 1000 characters should suffice to express their opinion :) */
+      />
+      {error && <p className="mt-2 text-red-500">{error}</p>}
+      <div className="flex flex-row space-x-2">
+        <button className="border border-solid font-semibold px-4 leading-10 rounded-md text-white border-gray-800 bg-gray-800 hover:border-gray-600 hover:bg-gray-600 active:border-gray-700 active:bg-gray-700 inline-flex my-4 w-36">
+          <p className="mx-auto" onClick={(e) => submitAnswer()}>
+            Submit
+          </p>
+        </button>
+        {submitted && <p className="text-green-500 my-auto">Your response has been submitted!</p>}
+      </div>
+    </div>
+  );
 };
 
 export default QuestionForm;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const { DB } = require("./configuration/DB");
 const { Logger } = require("./configuration/Logger");
@@ -17,6 +18,7 @@ const port = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => {
   Logger.info(`API is up in port ${port}, running in ${DB.MODE} mode`);
